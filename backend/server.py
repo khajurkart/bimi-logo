1202e96cee930bc36fb4648283b3f2cf55ceb50c
khajurkart <khajurkart@gmail.com>
2026-02-24 03:51:53 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -754,5 +754,5 @@ logging.basicConfig(
 async def shutdown_db_client():
     client.close()
 
-port = int(os.environ.get("PORT", 10000))
+#port = int(os.environ.get("PORT", 10000))
 #app.run(host="0.0.0.0", port=port)
